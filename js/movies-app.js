7df382a3648e90a985890c470ac20d2aa7630672
Johnny <mrtnz.jn@gmail.com>
2023-12-07 09:25:59 -0600

    created index.js to separate logic from function calls

--- js/movies-app.js
+++ js/movies-app.js
@@ -1,16 +1,15 @@
 "use strict";
 
 // ************ LOADER *******************
-// function loaderAnimations() {
+function loaderAnimations() {
+    window.addEventListener("load", () => {
+        let loadingDiv = document.getElementById("loadingDiv")
+        let body = document.getElementById("body")
+        body.style.visibility = "hidden"
+        loadingDiv.style.visibility = "visible"
+    })
+}
 
-window.addEventListener("load", () => {
-    let loadingDiv = document.getElementById("loadingDiv")
-    let body = document.getElementById("body")
-    body.style.visibility = "hidden"
-    loadingDiv.style.visibility = "visible"
-})
-
-// }
 function removeLoader() {
     let body = document.getElementById("body")
     let loadingDiv = document.getElementById("loadingDiv")
@@ -37,6 +36,7 @@ const loadMovies = async () => {
                         <img src="${movie.poster}" class="card-img-top mx-auto" style="width: 80%; height: 80%"
                              alt="...">
                         <div class="card-body">
+                            <input id="movieId" value="${movie.id}">
                             <p class="card-text" id="movie-dir"><b>Director:</b> ${movie.director}</p>
                             <p class="card-text" id="movie-year"><b>Year:</b> ${movie.year}</p>
                             <div class="d-flex justify-content-evenly">
@@ -56,6 +56,7 @@ const loadMovies = async () => {
         })
         document.getElementById("movie-cards").innerHTML = moviesHTML.join("");
 
+
     } catch (error) {
         console.log(error);
         document.getElementById("movie-cards").innerHTML = `<p style="color: white; font-size: 30px" class="d-flex justify-content-center">Umm.... Something went wrong! Our techs are working on it. Please try again later.</p>`;
@@ -65,5 +66,65 @@ const loadMovies = async () => {
     }
 }
 
-// ********** CALLING loadMovies FOR INITIAL LOADING OF MOVIES ************
-loadMovies();
+// *********** WHEN EDIT BTN IS THIS FUNCTION IS TRIGGERED AND popUpModal FUNCTION IS CALLED ************
+
+function popUpModal(id) {
+    fetch(`http://localhost:3000/movies/${id}`)
+        .then(resp => resp.json())
+        .then(movieData => {
+            console.log(movieData.id)
+            // movieData.title
+            populateEditModal(movieData);
+            document.getElementById("saveEdits").addEventListener("click", function () {
+                // e.preventDefault();
+                console.log("line 60: " + movieData.id)
+
+                fetch(`http://localhost:3000/movies/${movieData.id}`, {
+                    method: "PATCH",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(
+                        {
+                            id: movieData.id,
+                            title: document.getElementById("userEditedTitle").value,
+                            director: document.getElementById("userEditedDir").value,
+                            year: document.getElementById("userEditedYear").value,
+                            actors: document.getElementById("userEditedActors").value,
+                            plot: document.getElementById("userEditedPlot").value,
+                            genre: document.getElementById("userEditedGenre").value,
+                            rating: document.getElementById("userEditedRating").value,
+                            poster: document.getElementById("userEditedPoster").value
+
+                        }
+                    )
+                })
+                    .then(resp => resp.json())
+                    .then(movieData => console.log(movieData))
+                    .catch(error => console.error(error));
+
+                document.getElementById("myModal").modal('hide');
+                clearEditModal();
+                loadMovies();
+            })
+            document.getElementById("deleteMovie").addEventListener("click", function () {
+                // e.preventDefault();
+                deleteMovieById(movieData.id);
+                $('#myModal').modal('hide');
+                loadMovies();
+
+            })
+            document.getElementById("editCloseBtn").addEventListener("click", function () {
+                clearEditModal(movieData);
+                $('#myModal').modal('hide');
+
+            })
+        })
+}
+
+
+export {
+    loaderAnimations,
+    loadMovies,
+
+}
